61df71ad22b5c12b5a4d3f5275becc3979e755ee
Code Troops <info@codetroops.net>
2026-02-04 07:49:57 +0200

    Initial commit of project files

---
+++ Author Code Rider App V1.0.2/lib/config/constant.dart
@@ -0,0 +1,6 @@
+const String appName = 'Hyperlocal - Rider App';
+const String packageName = 'YOUR_APP_PACKAGE_NAME';
+const String domainBaseUrl = 'YOUR_DOMAIN_BASE_URL';
+const String baseUrl = '$domainBaseUrl/api/delivery-boy/';
+const String deliveryZoneUrl = '$domainBaseUrl/api/';
+const String defaultTheme = 'light';
